729cae987d9afe5e2b2271caabff12ee5190a76e
Purgso <jakob@spurgat.net>
2026-08-04 23:38:18 -0400

    added Lexus rebuild post

--- src/data/category.ts
+++ src/data/category.ts
@@ -1,12 +1,19 @@
 export interface Props {
   title: string;
   slug: string;
-  color: "green" | "blue" | "orange" | "purple" | "pink";
+  color: "green" | "blue" | "orange" | "purple" | "pink" | "yellow";
   description: string;
 }
 export type Category = Props;
 
 export const categories: Props[] = [
+  {
+    title: "Automotive",
+    slug: "automotive",
+    color: "yellow",
+    description:
+      "Projects that involve working on cars",
+  },
   {
     title: "Technology",
     slug: "technology",
